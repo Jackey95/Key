d021f2b0f81f7954c44048174f8365f8d6dc9d75
Jackey95 <395643714@qq.com>
2024-03-06 20:38:12 +0800

    第一次作业

---
+++ restful/src/main/java/com/example/restful/RestfulApplication.java
@@ -0,0 +1,16 @@
+package com.example.restful;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@MapperScan("com.example.restful.dao")
+public class RestfulApplication {
+    public static void main(String[] args) {
+        // 启动嵌入式的 Tomcat 并初始化 Spring 环境及其各 Spring 组件
+        SpringApplication.run(RestfulApplication.class, args);
+        System.out.println("程序正在运行...");
+    }
+}
---
+++ restful/src/main/java/com/example/restful/web/UserRestController.java
@@ -0,0 +1,50 @@
+package com.example.restful.web;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.restful.bean.User;
+import com.example.restful.service.UserService;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping(value = "/api/user")
+public class UserRestController {
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = "/user", method = RequestMethod.POST)
+    public boolean addUser( User user) {
+        System.out.println("开始新增...");
+        return userService.addUser(user);
+    }
+
+    @RequestMapping(value = "/user", method = RequestMethod.PUT)
+    public boolean updateUser( User user) {
+        System.out.println("开始更新...");
+        return userService.updateUser(user);
+    }
+
+    @RequestMapping(value = "/user", method = RequestMethod.DELETE)
+    public boolean delete(@RequestParam(value = "userName", required = true) int userId) {
+        System.out.println("开始删除...");
+        return userService.deleteUser(userId);
+    }
+
+
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    public User findByUserName(@RequestParam(value = "userName", required = true) String userName) {
+        System.out.println("开始查询...");
+        return userService.findUserByName(userName);
+    }
+
+
+    @RequestMapping(value = "/userAll", method = RequestMethod.GET)
+    public List<User> findByUserAge() {
+        System.out.println("开始查询所有数据...");
+        return userService.findAll();
+    }
+}
